e4000ad668ab483035b6ba2c6709b3bbf06c0d2b
Nha Tran <nha.tran@creants.net>
2017-10-09 15:45:29 +0700

    config db url

--- server/db/mongo/config.js
+++ server/db/mongo/config.js
@@ -2,7 +2,7 @@
 // 'mongodb://localhost/ReactWebpackNode'; export default {   db };
 
 const config = {
-  mongoURL: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost/newsdb',
+  mongoURL: process.env.MONGOHQ_URL || process.env.MONGODB_URI || (__PRODUCTION__ ? 'mongodb://bao-db-mongodb/newsdb' : 'mongodb://localhost/newsdb'),
   port: process.env.PORT || 8000,
   dev: process.env.NODE_ENV == 'development'
 };
